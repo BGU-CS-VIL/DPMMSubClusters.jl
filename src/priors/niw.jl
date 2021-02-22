50778b2c944e5b9771cbe4082024084d8ceb1d78
Or Dinari <dinari.or@gmail.com>
2021-02-22 19:55:11 +0200

    dd

--- src/priors/niw.jl
+++ src/priors/niw.jl
@@ -27,7 +27,7 @@ function calc_posterior(prior:: niw_hyperparams, suff_statistics::niw_sufficient
     ψ = (prior.ν * prior.ψ + prior.κ*prior.m*prior.m' -κ*m*m'+ suff_statistics.S) / ν
     ψ = Matrix(Hermitian(ψ))
     while(isposdef(ν*ψ) == false)
-        ψ += 0.1*I
+        ψ += 0.01*I
     end
     return niw_hyperparams(κ,m,ν,ψ)
 end
