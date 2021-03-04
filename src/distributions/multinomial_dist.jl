4cf3a09ef920a17699c5ae3b9b45c06e2083351b
Nick Judd <nclarkjudd@users.noreply.github.com>
2021-03-04 15:27:05 -0600

    Update multinomial_dist.jl
    typo in variable name

--- src/distributions/multinomial_dist.jl
+++ src/distributions/multinomial_dist.jl
@@ -5,11 +5,11 @@ using Distributions
     multinomial_hyper(α::AbstractArray{Float32,1})
 [Dirichlet Distribution](https://en.wikipedia.org/wiki/Dirichlet_distribution)
 """
-struct multinomial_dist <: distibution_sample
+struct multinomial_dist <: distribution_sample
     α::AbstractArray{Float32,1}
 end
 
 
-function log_likelihood!(r::AbstractArray,x::AbstractArray, distibution_sample::multinomial_dist , group::Int64 = -1)
-    r .= (distibution_sample.α' * x)[1,:]
+function log_likelihood!(r::AbstractArray,x::AbstractArray, distribution_sample::multinomial_dist , group::Int64 = -1)
+    r .= (distribution_sample.α' * x)[1,:]
 end
